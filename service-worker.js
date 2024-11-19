fdb658c4d9ade812131f7cae4eaf1847ab0cba0e
sparabu <sparabu@gmail.com>
2024-11-19 06:39:40 +0000

    updated index and service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,40 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/icon-192.png",
+    "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-          return response || fetch(event.request);
-      })
-  );
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
